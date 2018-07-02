775c1582ce425901e684270d6604596500481765
Neville <live.neville@live.com>
2018-07-02 13:28:24 -0400

    setup crud operations on task

--- TaskList/TaskList.MobileApp/TaskList.MobileApp/Pages/AddTaskPage.xaml.cs
+++ TaskList/TaskList.MobileApp/TaskList.MobileApp/Pages/AddTaskPage.xaml.cs
@@ -9,14 +9,16 @@ using TaskList.MobileApp.Consume;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using static TaskList.MobileApp.Consume.ConsumeSetup;
+
 namespace TaskList.MobileApp.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AddTaskPage : ContentPage
 	{
-		public AddTaskPage ()
-		{
-			InitializeComponent ();
+        public AddTaskPage(TaskItem task = null)
+        {
+            InitializeComponent();
 
             var handler = new ModelHandler<TaskItem>()
                             .Bind(t => t.Title, title, titleErr)
@@ -25,11 +27,40 @@ namespace TaskList.MobileApp.Pages
                             .Bind(t => t.EndTime, endDate, endTime)
                             .Bind(t => t.Completed, completed);
 
+            if (task == null)
+            {
+                btnEdit.IsVisible = false;
+                btnDelete.IsVisible = false;
+            }
+            else
+            {
+                btnAdd.IsVisible = false;
+                handler.SetModel(task);
+            }
+
             btnAdd.Clicked += async delegate {
                 await handler.SubmitAsync(
-                    async t => await ConsumeSetup.Consumer.PostAsync("api/TaskItems", t),
+                    async t => await Consumer.PostAsync("api/TaskItems", t),
                     async s => await Navigation.PushModalAsync(new MainPage())
                 );
+            };
+
+            btnEdit.Clicked += async delegate {
+                await handler.SubmitAsync(
+                    async t => await Consumer.PutAsync($"api/TaskItems/{t.Id}", t),
+                    async s => await Navigation.PushModalAsync(new MainPage())
+                );
+            };
+
+            btnDelete.Clicked += async delegate {
+                bool confirm = await DisplayAlert("Delete?", $"Are you sure you want to delete '{task.Title}'", "Delete", "No");
+                if (confirm)
+                {
+                    await handler.SubmitAsync(
+                        async t => await Consumer.DeleteAsync($"api/TaskItems/{t.Id}"),
+                        async s => await Navigation.PushModalAsync(new MainPage())
+                    );
+                }
             };
 
             btnHome.Clicked += async delegate {
